01176690df64898ce97da2ff19ccfae2eafdb245
Jeremy Zagorski <jmzagorski@gmail.com>
2017-05-08 10:31:56 -0400

    fix(npm): add http(s) since env var should not have it

--- dev_off.bat
+++ dev_off.bat
@@ -2,6 +2,7 @@ call npm config rm proxy
 call npm config rm https-proxy
 call git config --global --unset http.proxy
 call git config --global --unset https.proxy
+call nvm proxy none
 
 REG delete HKEY_CURRENT_USER\Environment /F /V http_proxy
 REG delete HKEY_CURRENT_USER\Environment /F /V https_proxy
