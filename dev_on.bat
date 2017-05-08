0d602b7068dd244c8a66d8b656ce8daac18e8436
Jeremy Zagorski <jmzagorski@gmail.com>
2017-05-08 10:31:37 -0400

    feat(nvm): turn off any nvm settings

--- dev_on.bat
+++ dev_on.bat
@@ -1,7 +1,8 @@
-call npm config set proxy %PROXY_WORK%
-call npm config set https-proxy %PROXY_WORK%
+call npm config set proxy http://%PROXY_WORK%
+call npm config set https-proxy http://%PROXY_WORK%
 call git config --global http.proxy %PROXY_WORK%
 call git config --global https.proxy %PROXY_WORK%
+call nvm proxy %PROXY_WORK%
 
 REG add HKEY_CURRENT_USER\Environment /F /V http_proxy /d %PROXY_WORK%
 REG add HKEY_CURRENT_USER\Environment /F /V https_proxy /d %PROXY_WORK%
